2d5fd800b640da7e09762e33e03078eeea81a81d
Chew <chew@chew.pw>
2022-02-11 13:12:47 -0600

    command handler?

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,50 @@
 #include <dpp/dpp.h>
 #include <iostream>
+#include <utility>
+#include "main.h"
+
+int main() {
+    // Start the program
+    dpp::cluster bot(getToken());
+
+    bot.on_ready([&bot](const auto &event) {
+        std::cout << "Logged in as " << bot.me.username << "!\n";
+    });
+
+    /* Create command handler, and specify prefixes */
+    dpp::commandhandler command_handler(&bot);
+    /* Specifying a prefix of "/" tells the command handler it should also expect slash commands */
+    command_handler.add_prefix("B%^");
+
+    bot.on_ready([&command_handler](const dpp::ready_t &event) {
+        command_handler.add_command("ping", {},
+            [&command_handler](const std::string &command, const dpp::parameter_list_t &parameters, dpp::command_source src) {
+                std::string got_param;
+                if (!parameters.empty()) {
+                    got_param = std::get<std::string>(parameters[0].second);
+                }
+                command_handler.reply(dpp::message("Pong! -> " + got_param), std::move(src));
+            },
+            "A test ping command",
+            148195924567392257
+        );
+
+        /**
+         * NOTE: We must call this to ensure slash commands are registered.
+         * This does a bulk register, which will replace other commands
+         * that are registered already!
+         */
+        command_handler.register_commands();
+
+    });
+
+    bot.start(false);
+    return 0;
+}
 
 std::string getToken() {
-    // Open a file with C++ and read it
-    FILE * fp = fopen("config.txt", "r");
+    // Open the config.txt file with C++ and read it
+    FILE *fp = fopen("config.txt", "r");
     if (fp == nullptr) {
         printf("The file was not found.\n");
         exit(1);
@@ -15,25 +56,7 @@ std::string getToken() {
     fclose(fp);
     // get everything after the token=
     strtok(line, "=");
-    char * token = strtok(nullptr, "=");
+    char *token = strtok(nullptr, "=");
     // Return the token
     return token;
-}
-
-int main() {
-    // Start the program
-    dpp::cluster bot(getToken());
-
-    bot.on_ready([&bot](const auto & event) {
-        std::cout << "Logged in as " << bot.me.username << "!\n";
-    });
-
-    bot.on_message_create([&bot](const auto & event) {
-        if (event.msg.content == "!ping") {
-            bot.message_create(dpp::message(event.msg.channel_id, "Pong!"));
-        }
-    });
-
-    bot.start(false);
-    return 0;
 }
---
+++ src/main.h
@@ -0,0 +1,15 @@
+//
+// Created by Chew on 2/10/22.
+//
+#include <iostream>
+
+#ifndef DISCORD_BOT_MAIN_H
+#define DISCORD_BOT_MAIN_H
+
+#endif //DISCORD_BOT_MAIN_H
+
+/**
+ * Gets the token from the config file
+ * @return the token
+ */
+std::string getToken();
